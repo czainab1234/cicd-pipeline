1b07dbd2f50a05994a1f3b371869733aad9855ac
czainab1234 <100843695+czainab1234@users.noreply.github.com>
2022-04-18 20:02:28 +0530

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,7 +2,7 @@
 
 ssh -tt ubuntu@18.169.189.7<<EOF
     cd ~/cicd-pipeline
-    git pull origin master
+    git pull origin Master
     curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
     . ~/.nvm/nvm.sh
     nvm install v12.4.0
