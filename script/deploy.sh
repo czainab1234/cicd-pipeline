c56c8257be98b41bdd0b1f9e107ad1678e653660
czainab1234 <100843695+czainab1234@users.noreply.github.com>
2022-04-18 17:55:47 +0530

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ssh -t ubuntu@18.169.189.7 <<EOF
+ssh -tt ubuntu@18.169.189.7 <<EOF
     cd ~/cicd-pipeline
     git pull origin master
     curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
