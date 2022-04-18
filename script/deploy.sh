401ae030477e0c4684c9e0fc9180f808f6068e34
czainab1234 <100843695+czainab1234@users.noreply.github.com>
2022-04-18 20:32:32 +0530

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -5,7 +5,7 @@ ssh -tt ubuntu@18.169.189.7<<EOF
     git pull origin Master
     curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
     . ~/.nvm/nvm.sh
-    nvm install v12.4.0
+    nvm install v10.11.0
     npm install
     npm install pm2 -g
     pm2 start ecosystem.config.js
