bfc744225d6a28491ef58c2ebd78f73dda21dd6d
czainab1234 <100843695+czainab1234@users.noreply.github.com>
2022-04-18 22:03:48 +0530

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,8 +3,8 @@
 ssh -tt ubuntu@18.169.189.7<<EOF
     cd ~/cicd-pipeline
     git pull origin Master
-    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-    . ~/.nvm/nvm.sh
+    $ curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+    source ~/.nvm/nvm.sh
     nvm install v10.11.0
     npm install
     npm install pm2 -g
