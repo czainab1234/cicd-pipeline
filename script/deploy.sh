cd060525a5c82074b2df2e344e6b5d7fd2361bae
czainab1234 <100843695+czainab1234@users.noreply.github.com>
2022-04-19 15:40:52 +0530

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -8,7 +8,8 @@ ssh -tt ubuntu@13.234.115.140<<EOF
     nvm install v10.11.0
     npm install
     npm install pm2 -g
-    pm2 restart index.js
+    
+    pm2 restart ecosystem.config.js
     
    
     
