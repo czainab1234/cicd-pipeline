e148f82b04bcf6100521e94c604bf544277e00b9
czainab1234 <100843695+czainab1234@users.noreply.github.com>
2022-04-19 14:40:47 +0530

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,12 +3,12 @@
 ssh -tt ubuntu@13.234.115.140<<EOF
     cd ~/cicd-pipeline
     git pull origin Master
-    $ curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+    $ curl -o-   https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh    | bash
     source ~/.nvm/nvm.sh
     nvm install v10.11.0
     npm install
     npm install pm2 -g
-    pm2 start ecosystem.config.js
+    pm2 restart ecosystem.config.js
    
     exit
 EOF
